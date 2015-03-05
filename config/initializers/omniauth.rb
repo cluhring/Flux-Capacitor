7c683241919e2460721b162f014ea56aab8ff1f0
Chris Luhring <cluhring@gmail.com>
2015-03-04 23:26:01 -0700

    figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_key'], ENV['facebook_secret']
+  provider :facebook, Figaro.env.facebook_key, Figaro.env.facebook_secret
 end
