130dd7d302a7ab45133398b0e712858fa10d1370
Chris Luhring <cluhring@gmail.com>
2015-03-08 10:37:26 -0600

    css looking better

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,22 +1,18 @@
 $(document).ready(function(){
 
+  $("div.map").height( $(".routing").height() );
+
   $("#aa").click(function(){
-    $('#map_address1').after("<p id='stopping_point'><br><label for='Starting_Point'>Stopping point</label><input type='text' class='input-large' name='map_address' id='map_address' value='Address, City, State, and/or Zip' onclick='if($(this).val()==&quot;Address, City, State, and/or Zip&quot;){$(this).val(&quot;&quot;);};''>"+
-    '<button id="removeStoppingPoint" type="button">remove</button></div></p>');
+    $('#map_address1').after("<p id='stopping_point'><label for='Starting_Point'>Stopping point</label><input type='text' class='input-large' name='map_address' id='map_address' value='Address, City, State, and/or Zip' onclick='if($(this).val()==&quot;Address, City, State, and/or Zip&quot;){$(this).val(&quot;&quot;);};''>"+
+    '<button id="removeStoppingPoint" type="button">remove</button></p>');
   });
 
-  $("#removeStoppingPoint").on('click', function(){
-    $("p").remove("#stopping_point");
-  });
+  // $("#removeStoppingPoint").on('click', function(){
+  //   $("p").remove("#stopping_point");
+  // });
 
 
 });
-
-
-  //
-  // $("#aa").click(function(){
-  //   $("<input type='text' class='input-large' name='map_address' id='map_address' value='Address, City, State, and/or Zip' onclick='if($(this).val()==&quot;Address, City, State, and/or Zip&quot;){$(this).val(&quot;&quot;);};''>").appendTo('#map_address1');
-  // });
 
 
 // $('#bb').click(function(){
